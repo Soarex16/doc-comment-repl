d5c0624383dac31d9b0710b45f2bf9dc25d804db
GitHub Action <action@github.com>
2021-11-29 06:19:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/soarex16/doccommentrepl/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/soarex16/doccommentrepl/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.soarex16.doccommentrepl.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.soarex16.doccommentrepl.MyBundle
 
 class MyApplicationService {
 
