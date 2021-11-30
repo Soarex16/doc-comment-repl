2f2e272d3bfa255fa078b509ec2eb44b70b1cb85
soarex <soarex16@gmail.com>
2021-11-30 20:58:13 +0300

    Scratch executor

--- src/main/kotlin/com/github/soarex16/doccommentrepl/ExecuteSnippetAction.kt
+++ src/main/kotlin/com/github/soarex16/doccommentrepl/ExecuteSnippetAction.kt
@@ -1,33 +1,62 @@
 package com.github.soarex16.doccommentrepl
 
 import com.github.soarex16.doccommentrepl.ui.COMMENT_NODE_TYPES
+import com.intellij.ide.scratch.ScratchFileService
+import com.intellij.ide.scratch.ScratchRootType
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.ex.util.EditorUtil
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.PsiParserFacade
 import com.intellij.psi.SmartPsiElementPointer
 import com.intellij.psi.util.elementType
 import org.jetbrains.kotlin.KotlinIdeaReplBundle
 import org.jetbrains.kotlin.console.actions.errorNotification
+import org.jetbrains.kotlin.idea.KotlinLanguage
+import org.jetbrains.kotlin.idea.scratch.*
+import org.jetbrains.kotlin.idea.scratch.compile.KtCompilingExecutor
+import org.jetbrains.kotlin.idea.scratch.output.ScratchOutput
+import org.jetbrains.kotlin.idea.scratch.output.ScratchOutputHandler
+import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.psiUtil.endOffset
 import org.jetbrains.kotlin.script.jsr223.KotlinJsr223JvmLocalScriptEngineFactory
+import java.io.File
 import javax.script.ScriptEngineManager
 
-/**
- * >>> 1 + 2
- * res0: kotlin.Int = 3
- *
- * some text
- *
- * >>> "aBc".toUpperCase()
- * res1: kotlin.String = ABC
- */
+class SnippetResultHandler(val project: Project, val doc: Document, val element: PsiElement): ScratchOutputHandler {
+    override fun clear(file: ScratchFile) {
+
+    }
+
+    override fun error(file: ScratchFile, message: String) {
+
+    }
+
+    override fun handle(file: ScratchFile, expression: ScratchExpression, output: ScratchOutput) {
+        WriteCommandAction.runWriteCommandAction(project) {
+            val commentString = output.text.lines().joinToString { "\n//$it" }
+            doc.insertString(element.textRange.endOffset, commentString)
+        }
+    }
+
+    override fun onFinish(file: ScratchFile) {
+
+    }
+
+    override fun onStart(file: ScratchFile) {
+
+    }
+}
+
 class ExecuteSnippetAction(val code: String, private val callElement: SmartPsiElementPointer<PsiElement>?, val snippetTextRange: TextRange) : AnAction() {
     constructor() : this("", null, TextRange.EMPTY_RANGE)
 
@@ -50,14 +79,15 @@ class ExecuteSnippetAction(val code: String, private val callElement: SmartPsiEl
 
         val module = ProjectRootManager.getInstance(project).fileIndex.getModuleForFile(activeDocFile!!)!!
 
-        val executionResult = "abc"
+        val vFile = PsiFileFactory
+                .getInstance(project)
+                .createFileFromText(psiElement.language, code)
+                .virtualFile
 
-        val commentString = executionResult.lines().joinToString { "\n//$it" }
-        WriteCommandAction.runWriteCommandAction(project) {
-            activeDocument.insertString(psiElement.textRange.endOffset, "$commentString\n")
-        }
-        /*val commentElement = createComment(psiElement, executionResult)
+        val scratch = KtScratchFileLanguageProvider().newScratchFile(project, vFile)
 
-        psiElement.parent.addAfter(commentElement, psiElement)*/
+        val executor = scratch?.compilingScratchExecutor ?: return
+        executor.addOutputHandler(SnippetResultHandler(project, activeDocument, psiElement))
+        executor.execute()
     }
 }
