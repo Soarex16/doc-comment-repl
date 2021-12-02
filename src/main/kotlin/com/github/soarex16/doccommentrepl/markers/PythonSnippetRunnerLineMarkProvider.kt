4bd365e3e0ddcc2d1dabc5e37df4236a4a78ac7e
soarex <soarex16@gmail.com>
2021-12-02 03:47:03 +0300

    test python + kotlin compatibility

--- src/main/kotlin/com/github/soarex16/doccommentrepl/markers/PythonSnippetRunnerLineMarkProvider.kt
+++ src/main/kotlin/com/github/soarex16/doccommentrepl/markers/PythonSnippetRunnerLineMarkProvider.kt
@@ -1,13 +1,22 @@
 package com.github.soarex16.doccommentrepl.markers
 
 import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.elementType
+import com.jetbrains.python.PyTokenTypes
 
 /**
  * Провайдер маркеров запуска сниппетов кода в Питоне
  */
 class PythonSnippetRunnerLineMarkProvider : ExecuteSnippetRunnerLineMarkProvider() {
     override fun extractCodeSnippet(element: PsiElement): Pair<String, TextRange>? {
-        TODO("Not yet implemented")
+        val elType = element.elementType
+        // PyTokenTypes.END_OF_LINE_COMMENT
+        // PyTokenTypes.DOCSTRING
+
+        val isComment = element is PsiComment
+
+        return null
     }
 }
